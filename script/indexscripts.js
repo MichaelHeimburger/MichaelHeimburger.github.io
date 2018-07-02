4f455fdd5eed770d7c1b3bab9f3e00a2625fc47c
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-02 07:39:05 -0400

    initial commit

---
+++ script/indexscripts.js
@@ -0,0 +1,13 @@
+﻿function GetBG() {
+    var x = Math.floor((Math.random() * 10) + 1);
+    var vid = document.getElementById("bgvid");
+    if (x < 4) {
+        vid.src = "images/waves.mp4";
+    }
+    else if (x < 8) {
+        vid.src = "images/waves2.mp4";
+    }
+    else {
+        vid.src = "images/waves3.mp4";
+    }
+}
