566b06b8dca908d24133111f3ac6c88427efa996
MichaelHeimburger <mrheimburger45@gmail.com>
2018-07-17 18:23:44 -0400

    updated site to add another project, started info divswap

--- script/indexscripts.js
+++ script/indexscripts.js
@@ -12,33 +12,57 @@
     }
 }
 
-function InfoSTK() {
-    document.getElementsByClassName('infowin')[0].style.display = "flex";
-    document.getElementsByClassName('center')[0].style.display = "flex";
-    document.getElementsByClassName('stkmktinfo')[0].style.display = "block";
-}
-function InfoVPS() {
-    document.getElementsByClassName('infowin')[0].style.display = "flex";
-    document.getElementsByClassName('center')[0].style.display = "flex";
-    document.getElementsByClassName('Virtualpetinfo')[0].style.display = "block";
-}
-function hideinfo() {
+    function SkillsSwap() {
+        alert('t');
+        document.getElementById("maininfo").style.display = "none";
+        document.getElementById("skillinfo").style.display = "flex";
+        //document.getElementById("active").style.zIndex = "-1";
+        //document.getElementById("nonactive").style.zIndex = "1";
+    }
+    function InfoSwap() {
+        alert('t');
+        document.getElementById("skillinfo").style.display = "none";
+        document.getElementById("maininfo").style.display = "flex";
+        //document.getElementById("active").style.zIndex = "1";
+        //document.getElementById("nonactive").style.zIndex = "-1";
+    }
 
-    document.getElementsByClassName("infowin")[0].style.display="none";
-    document.getElementsByClassName('center')[0].style.display = "none";
-    document.getElementsByClassName('stkmktinfo')[0].style.display = "none";
-    document.getElementsByClassName('Virtualpetinfo')[0].style.display = "none";
-    document.getElementsByClassName('tucanyvillainfo')[0].style.display = "none";
-    document.getElementsByClassName('michaelsoftinfo')[0].style.display = "none";
 
+
+
+    function InfoSTK() {
+        document.getElementsByClassName('infowin')[0].style.display = "flex";
+        document.getElementsByClassName('center')[0].style.display = "flex";
+        document.getElementsByClassName('stkmktinfo')[0].style.display = "block";
+    }
+    function InfoVPS() {
+        document.getElementsByClassName('infowin')[0].style.display = "flex";
+        document.getElementsByClassName('center')[0].style.display = "flex";
+        document.getElementsByClassName('Virtualpetinfo')[0].style.display = "block";
+    }
+    function hideinfo() {
+
+        document.getElementsByClassName("infowin")[0].style.display = "none";
+        document.getElementsByClassName('center')[0].style.display = "none";
+        document.getElementsByClassName('stkmktinfo')[0].style.display = "none";
+        document.getElementsByClassName('Virtualpetinfo')[0].style.display = "none";
+        document.getElementsByClassName('tucanyvillainfo')[0].style.display = "none";
+        document.getElementsByClassName('michaelsoftinfo')[0].style.display = "none";
+        document.getElementsByClassName('citymvcinfo')[0].style.display = "none";
+
+    }
+    function InfoTUS() {
+        document.getElementsByClassName('infowin')[0].style.display = "flex";
+        document.getElementsByClassName('center')[0].style.display = "flex";
+        document.getElementsByClassName('tucanyvillainfo')[0].style.display = "block";
+    }
+    function InfoMSO() {
+        document.getElementsByClassName('infowin')[0].style.display = "flex";
+        document.getElementsByClassName('center')[0].style.display = "flex";
+        document.getElementsByClassName('michaelsoftinfo')[0].style.display = "block";
 }
-function InfoTUS() {
+function InfoCTY() {
     document.getElementsByClassName('infowin')[0].style.display = "flex";
     document.getElementsByClassName('center')[0].style.display = "flex";
-    document.getElementsByClassName('tucanyvillainfo')[0].style.display = "block";
-}
-function InfoMSO() {
-    document.getElementsByClassName('infowin')[0].style.display = "flex";
-    document.getElementsByClassName('center')[0].style.display = "flex";
-    document.getElementsByClassName('michaelsoftinfo')[0].style.display = "block";
+    document.getElementsByClassName('citymvcinfo')[0].style.display = "block";
 }
